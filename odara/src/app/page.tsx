8a36fd472b6a3e30c2502d876cea43cc7c7eee3d
Samuel Olusanya <95101113+Jester8@users.noreply.github.com>
2026-05-03 10:31:44 +0100

    correction to navbar

--- odara/src/app/page.tsx
+++ odara/src/app/page.tsx
@@ -2,7 +2,7 @@
 import Navbar from "@/components/ui/Navbar";
 import Hero from "@/components/ui/Hero";
 import Products from "@/components/layout/Products";
-import Footer from "@/components/ui/Footer"
+import Footer from "@/components/ui/footer"
 
 export default function Home() {
   return (
