e5493447e8e9a2a97b452134277d204cbbb9f7bc
Samuel Olusanya <95101113+Jester8@users.noreply.github.com>
2026-05-04 10:08:18 +0100

    nsvb

--- odara/src/app/page.tsx
+++ odara/src/app/page.tsx
@@ -3,11 +3,12 @@
 import Hero from "@/components/ui/Hero";
 import Products from "@/components/layout/Products";
 import Footer from "@/components/layout/Footer"
+import Navbar from "@/components/ui/Navbar";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-    
+       <Navbar />
       <Hero />
       <Products />
       <Footer/>
